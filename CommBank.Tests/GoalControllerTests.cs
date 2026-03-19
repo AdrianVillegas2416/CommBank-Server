c0e09dfc11154329e7111b8011a266042a1a2794
Adrian Villegas <ad412437@dal.ca>
2026-03-18 22:11:14 -0300

    Add GetForUser unit test and update test project to net10.0

--- CommBank.Tests/GoalControllerTests.cs
+++ CommBank.Tests/GoalControllerTests.cs
@@ -65,10 +65,22 @@ public class GoalControllerTests
     [Fact]
     public async void GetForUser()
     {
-        // Arrange
-        
-        // Act
-        
-        // Assert
+         // Arrange
+    var goals = collections.GetGoals();
+    var users = collections.GetUsers();
+    IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
+    IUsersService usersService = new FakeUsersService(users, users[0]);
+    GoalController controller = new(goalsService, usersService);
+
+    // Act
+    var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+    controller.ControllerContext.HttpContext = httpContext;
+    var result = await controller.GetForUser(users[0].Id!);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.IsAssignableFrom<List<Goal>>(result);
+    Assert.Equal(goals[0].Id, result![0].Id);
+    Assert.Equal(goals[0].Name, result![0].Name);
     }
 }
